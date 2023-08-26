5a79b0e16d9590895ad915b525bdfcc65e190213
virajnikam01 <virajnikam01@gmail.com>
2023-08-26 19:36:24 +0530

    added the sectio for skills and improved the fonts

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-var crsr = document.querySelector("#curser");
+// var crsr = document.querySelector("#curser");
 
-document.addEventListener("mousemove", function(details){
+// document.addEventListener("mousemove", function(details){
 
-    crsr.style.left = details.x+"px";
-    crsr.style.top = details.y+"px";
-})
+//     crsr.style.left = details.x+"px";
+//     crsr.style.top = details.y+"px";
+// })
 
-var tl = gsap.timeline()
+// var tl = gsap.timeline()
 
-tl.from("#nav h3, #navbar ",{
-    y:-50,
-    delay:0.6,
-    opacity: 0,
-    stagger: 0.1
-})
+// tl.from("#nav h3, #navbar ",{
+//     y:-50,
+//     delay:0.6,
+//     opacity: 0,
+//     stagger: 0.1
+// })
 
-tl.from("#content h1",{
-    scale:2,
-    opacity:0,
-    color:"black"
-})
-tl.from("#history p",{
-    y: 50,
-    opacity:0
-})
-tl.from("#history i",{
-    opacity:0,
-    y:-20,
-    repeat:-1,
-    yoyo:true
-})
-tl.from("#about",{
-    scale:0,
-    opacity:0
-})
-tl.from("#work h3",{
-    scale:0,
-    opacity:0,
-})
+// tl.from("#content h1",{
+//     scale:2,
+//     opacity:0,
+//     color:"black"
+// })
+// tl.from("#history p",{
+//     y: 50,
+//     opacity:0
+// })
+// tl.from("#history i",{
+//     opacity:0,
+//     y:-20,
+//     repeat:-1,
+//     yoyo:true
+// })
+// tl.from("#about",{
+//     scale:0,
+//     opacity:0
+// })
+// tl.from("#work h3",{
+//     scale:0,
+//     opacity:0,
+// })
 
-tl.from("#cards",{
-    opacity:0,
-    x:-1000,
-    duraion:0.5,
+// tl.from("#cards",{
+//     opacity:0,
+//     x:-1000,
+//     duraion:0.5,
 
-})
+// })
