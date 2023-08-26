98482cba8d97f776cdf84007859bbba0b5962469
virajnikam01 <virajnikam01@gmail.com>
2023-08-27 00:20:26 +0530

    added smooth scrolling

--- script.js
+++ script.js
@@ -1,47 +1,63 @@
-// var crsr = document.querySelector("#curser");
+function loco() {
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('#main'),
+        smooth: true
+    });
+}
+loco()
 
-// document.addEventListener("mousemove", function(details){
 
-//     crsr.style.left = details.x+"px";
-//     crsr.style.top = details.y+"px";
-// })
+var crsr = document.querySelector("#curser");
 
-// var tl = gsap.timeline()
+document.addEventListener("mousemove", function(details){
 
-// tl.from("#nav h3, #navbar ",{
-//     y:-50,
-//     delay:0.6,
-//     opacity: 0,
-//     stagger: 0.1
-// })
+    crsr.style.left = details.x+"px";
+    crsr.style.top = details.y+"px";
+})
 
-// tl.from("#content h1",{
-//     scale:2,
-//     opacity:0,
-//     color:"black"
-// })
-// tl.from("#history p",{
-//     y: 50,
-//     opacity:0
-// })
-// tl.from("#history i",{
-//     opacity:0,
-//     y:-20,
-//     repeat:-1,
-//     yoyo:true
-// })
-// tl.from("#about",{
-//     scale:0,
-//     opacity:0
-// })
-// tl.from("#work h3",{
-//     scale:0,
-//     opacity:0,
-// })
+var tl = gsap.timeline()
 
-// tl.from("#cards",{
-//     opacity:0,
-//     x:-1000,
-//     duraion:0.5,
+tl.from("#nav button",{
+    opacity:0,
+    x:10000,
+    duration:0.8
+ })
+gsap.from("#nav h3",{
+    opacity:0,
+    x:-1000,
+    // duration:0.8
+ })
+ tl.from("#page1 h1",{
+    opacity:0,
+    x:1000,
+    // duration:0.8
+ })
+ tl.from("#page1 span",{
+    opacity:0,
+    // duration:0.5
+ })
+gsap.from("#page1 img",{
+    opacity:0,
+    x:-1000,
+    duration:0.7
+ })
+ tl.from("#history",{
+    opacity:0,
+    y:100
+    
+ })
+ tl.from("#history i",{
+    opacity:0,
+    y:-10,
+    repeat:-1,
+    yoyo:true
+ })
+ tl.from("#page2 p",{
+    opacity:0,
+    y:1000,
+    stagger:0.1
+ })
 
-// })
+
+
+
